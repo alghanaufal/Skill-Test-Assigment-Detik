e918e6bdf37831c04e7e05426b72f9833249a547
algha3 <anaufalhz03@gmail.com>
2023-12-30 22:07:38 +0700

    beta

--- js/script.js
+++ js/script.js
@@ -12,12 +12,12 @@ var swiper = new Swiper(".mySwiper", {
   },
   breakpoints: {
     320: {
-      slidesPerView: 2,
-      spaceBetween: 20,
+      slidesPerView: 1.5,
+      spaceBetween: 5,
     },
     586: {
-      slidesPerView: 3,
-      spaceBetween: 40,
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
     1440: {
       slidesPerView: 4,
